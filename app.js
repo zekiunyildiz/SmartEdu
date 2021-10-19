f756a504c12ff3ba3852a98c968ae51aa4ab2b59
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-19 04:03:51 +0300

    project link

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
+//Template engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
+
+//Routes
+
 app.get('/', (req, res) => {
-  res.send('INDEX SAYFASI');
+  res.status(200).render('index', {
+    page_name: 'index'
+  })
 });
+
+app.get('/about', (req, res)=>{
+  res.status(200).render('about', {
+    page_name:'about'
+  })
+})
 
 const port = 3000;
 

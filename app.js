b47622638d4d4649d7f4c0da41e03c4c57b89169
zekiunyildiz <zekiunyildiz@gmail.com>
2021-10-20 02:04:39 +0300

    routing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pageRoute = require('./routes/pageRoute')
 
 const app = express();
 
@@ -9,18 +10,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 //Routes
+app.use('/', pageRoute);
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-    page_name: 'index'
-  })
-});
-
-app.get('/about', (req, res)=>{
-  res.status(200).render('about', {
-    page_name:'about'
-  })
-})
 
 const port = 3000;
 
